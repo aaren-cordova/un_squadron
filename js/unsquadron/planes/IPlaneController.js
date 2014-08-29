316a309f7e053e778762e348658f4410aaa036b5
aaren-cordova <aaren.cordova@demandmedia.com>
2014-08-29 15:42:22 -0700

    Added plane controller interface

--- js/unsquadron/planes/IPlaneController.js
+++ js/unsquadron/planes/IPlaneController.js
@@ -1,10 +1,50 @@
-goog.require('goog.asserts');
-
-goog.provide('unsquadron.planes.IPlaneController')
 goog.scope(function(){
+	goog.require('goog.disposable.Disposable');
+	goog.provide('unsquadron.planes.IPlaneController')
+	
 	/** 
 	* @interface
 	* @extends {goog.disposable.Disposable}
 	*/
 	unsquadron.planes.IPlaneController = function(){};
+
+	// L
+	unsquadron.planes.IPlaneController.prototype.onPreviousSpecialWeaponButtonDown = function(event){};
+	unsquadron.planes.IPlaneController.prototype.onPreviousSpecialWeaponButtonUp = function(event){};
+
+	// R
+	unsquadron.planes.IPlaneController.prototype.onNextSpecialWeaponButtonDown = function(event){};
+	unsquadron.planes.IPlaneController.prototype.onNextSpecialWeaponButtonUp = function(event){};
+
+	// Y / X
+	unsquadron.planes.IPlaneController.prototype.onUseStandardWeaponButtonDown = function(event){};
+	unsquadron.planes.IPlaneController.prototype.onUseStandardWeaponButtonUp = function(event){};
+
+	// B / A
+	unsquadron.planes.IPlaneController.prototype.onUseSpecialWeaponButtonDown = function(event){};
+	unsquadron.planes.IPlaneController.prototype.onUseSpecialWeaponButtonUp = function(event){};
+
+	// <
+	unsquadron.planes.IPlaneController.prototype.onLeftButtonDown = function(event){};
+	unsquadron.planes.IPlaneController.prototype.onLeftButtonUp = function(event){};
+
+	// >
+	unsquadron.planes.IPlaneController.prototype.onRightButtonDown = function(event){};
+	unsquadron.planes.IPlaneController.prototype.onRightButtonUp = function(event){};
+
+	// ^
+	unsquadron.planes.IPlaneController.prototype.onUpButtonDown = function(event){};
+	unsquadron.planes.IPlaneController.prototype.onUpButtonUp = function(event){};
+
+	// v
+	unsquadron.planes.IPlaneController.prototype.onDownButtonDown = function(event){};
+	unsquadron.planes.IPlaneController.prototype.onDownButtonUp = function(event){};
+
+	// pause
+	unsquadron.planes.IPlaneController.prototype.onPauseButtonDown = function(event){};
+	unsquadron.planes.IPlaneController.prototype.onPauseButtonUp = function(event){};
+
+	// select
+	unsquadron.planes.IPlaneController.prototype.onSelectButtonDown = function(event){};
+	unsquadron.planes.IPlaneController.prototype.onSelectButtonUp = function(event){};
 });

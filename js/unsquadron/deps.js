32d0cada4fb47065cbaf40553f97167d1cd08b73
aaren-cordova <aaren.cordova@demandmedia.com>
2014-09-13 18:47:49 -0700

    Added level 1 psd

--- js/unsquadron/deps.js
+++ js/unsquadron/deps.js
@@ -30,7 +30,7 @@ goog.addDependency('../../../../unsquadron/display/turrent0.js', ['unsquadron.di
 goog.addDependency('../../../../unsquadron/display/turrent1.js', ['unsquadron.display.Turrent1'], ['goog.Timer', 'goog.math.Coordinate', 'unsquadron.display.Bullet1', 'unsquadron.display.Turrent']);
 goog.addDependency('../../../../unsquadron/display/weapon.js', ['unsquadron.display.Weapon'], []);
 goog.addDependency('../../../../unsquadron/game.js', ['unsquadron.Game', 'unsquadron.GameLayer'], ['goog.Timer', 'goog.asserts', 'qcurve.display.Component', 'unsquadron.display.Bullet']);
-goog.addDependency('../../../../unsquadron/main.js', ['unsquadron.Main'], ['qcurve.display.Sprite', 'qcurve.display.Stage', 'unsquadron.Game', 'unsquadron.display.Bullet0', 'unsquadron.display.Helicopter', 'unsquadron.display.Tank0', 'unsquadron.display.Turrent0', 'unsquadron.display.Turrent1']);
+goog.addDependency('../../../../unsquadron/src/main.js', ['unsquadron.Main'], ['qcurve.display.Sprite', 'qcurve.display.Stage', 'unsquadron.Game', 'unsquadron.display.Bullet0', 'unsquadron.display.Helicopter', 'unsquadron.display.Tank0', 'unsquadron.display.Turrent0', 'unsquadron.display.Turrent1']);
 goog.addDependency('../../../../unsquadron/planes/IPlaneController.js', ['unsquadron.planes.IPlaneController'], ['goog.asserts']);
 goog.addDependency('../../../../unsquadron/planes/IPlaneModel.js', ['unsquadron.planes.IPlaneModel'], ['goog.asserts']);
 goog.addDependency('../../../../unsquadron/players/IPlayerController.js', ['unsquadron.players.IPlayerController'], ['goog.asserts']);

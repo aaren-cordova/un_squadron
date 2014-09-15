274834be40d9ca7a584d224f7621107519c201b3
aaren-cordova <aaren.cordova@demandmedia.com>
2014-09-15 12:52:07 -0700

    Updated example 2.0 format

--- blog/2.0-whitespace-mode.compile.js
+++ blog/2.0-whitespace-mode.compile.js
@@ -6,27 +6,25 @@
 // Example 2.0 - Whitespace mode
 
 /**
-* @license
-* Property of Demand Media, all rights reserved
-* 
-* All of Googles Closure Tools are under the Apache License, Version 2.0
-* Subject to the terms and conditions of this License, each Contributor hereby grants to You 
-* a perpetual, worldwide, non-exclusive, no-charge, royalty-free, irrevocable copyright license to 
-* reproduce, prepare Derivative Works of, publicly display, publicly perform, sublicense, and 
-* distribute the Work and such Derivative Works in Source or Object form
+ * @license
+ * Property of Demand Media, all rights reserved
+ * 
+ * All of Googles Closure Tools are under the Apache License, Version 2.0
+ * Subject to the terms and conditions of this License, each Contributor hereby grants to You 
+ * a perpetual, worldwide, non-exclusive, no-charge, royalty-free, irrevocable copyright license to 
+ * reproduce, prepare Derivative Works of, publicly display, publicly perform, sublicense, and 
+ * distribute the Work and such Derivative Works in Source or Object form
 */
 
 // All work
 // and no play
+// makes Jack
+// a dull boy.
 
 /**
-* @preserve
-* Don't be evil.
+ * @preserve
+ * Don't be evil.
 */
-
-// makes Jack
-// a dull boy
-
 
 /**
  * All work and no play makes Jack a dull boy.
@@ -34,7 +32,7 @@
  * All work and play makes Jck a dull boy.
  * All work and no ply makes Jack a dull boy.
  * Aol work and no play makes Jack a dull boy.
- *   All work and no play makes Jack a dull boy.
+ *  All work and no play makes Jack a dull boy.
  * All work and no play makes Jack adull boy.
  * 
  * @param  {number} player_number

919c89eca2276a856df666db1bfbf76404e529fb
aaren-cordova <aaren.cordova@demandmedia.com>
2014-09-15 14:37:24 -0700

    Updated 3.1 demo to clarify dead code

--- blog/3.1-simple-mode.removal-of-dead-code.compile.js
+++ blog/3.1-simple-mode.removal-of-dead-code.compile.js
@@ -4,50 +4,57 @@
 // @formatting pretty_print
 // ==/ClosureCompiler==
 
-// Removal of dead code
+// Example 3.0 - Simple Mode - Removal of dead code
+
 console.log("Mario starts his quest.");
 
-function throw_dead_code(){
+function zombie_apocalypse(){
+	console.log(
+		"Zombie Bowser eat's Mario." + 
+		"The mushroom kingdom is doomed!" +
+		"In the land of the dead, Big Boo is king."
+	);
+}
+
+function throw_example(){
 	console.log("Mario beats the castle . . .");
 	throw new Error("I'm sorry but the princess is in another castle");
 
 	//Dead code
-	console.log("Bowser defeats Mario.");
+	zombie_apocalypse("Bowser defeats Mario.");
 };
 
-function break_dead_code(world){
+function break_example(world){
 	while(true){
 		console.log("he takes warp pipe directly to world " + world + "...");
 		break;
 
 		//Dead code
-		console.log("Bowser defeats Mario.");
+		zombie_apocalypse("Bowser defeats Mario.");
 	}
 };
 
-function return_dead_code(){
+function return_example(){
 	console.log("There's a face off with Bowser . . .");
 	return;
 
 	//Dead code
-	console.log("Bowser defeats Mario.");
+	zombie_apocalypse("Bowser defeats Mario.");
 };
 
-
-
-function continue_dead_code(){
+function continue_example(){
 	console.log("He uses his jumping skills . . .");
 	for(var i = 0; i < 100; ++i){
 		console.log("Mario saves the princess . . .");
 		continue;
 
 		//Dead code
-		console.log("Bowser defeats Mario.");
+		zombie_apocalypse("Bowser defeats Mario.");
 	}
 };
 
-break_dead_code(8);
-return_dead_code();
-continue_dead_code();
+break_example(8);
+return_example();
+continue_example();
 
 console.log("Want to play again?");
